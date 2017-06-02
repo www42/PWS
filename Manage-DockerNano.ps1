7fa37bf8c3b550cbb3f1cf42ae9fae2cc6f9a8bc
www42 <Wilhelm.Wien@outlook.de>
2017-06-02 18:32:04 +0200

    FW rule with PowerShell

--- Manage-DockerNano.ps1
+++ Manage-DockerNano.ps1
@@ -2,9 +2,16 @@
 #
 #   netsh advfirewall firewall add rule name="Docker daemon" dir=in action=allow protocol=TCP localport=2375
 
+New-NetFirewallRule -Name "Allow Docker daemon" `
+                    -DisplayName "Allow Docker daemon" `
+                    -Direction Inbound `
+                    -Action Allow `
+                    -LocalPort 2375 `
+                    -Protocol tcp `
+                    -Profile Any
 
 # Create Docker daemon config file
-$DockerConfig = 'C:\ProgramData\Docker\config\daemon.json'
+$DockerConfig = "$env:ProgramData\Docker\config\daemon.json"
 New-Item -ItemType File -Path $DockerConfig
 
 Add-Content -Path $DockerConfig -Value '{ "hosts": ["tcp://0.0.0.0:2375", "npipe://"] }'
